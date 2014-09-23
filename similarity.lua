0ae3bc753d05b470f61edb9b7bafdde4fc7fd1d4
smltng <samtung@rrdhcp-10-32-143-194.redrover.cornell.edu>
2014-09-23 02:42:21 -0400

    Cosine Similarity
    Messing around with Lua/Github

---
+++ similarity.lua
@@ -0,0 +1,14 @@
+--A pretty generic implementation of cosine similarity
+function getSimilarity(coordA, coordB)
+	local sum = 0
+	local magnitudeA = 0
+	local magnitudeB = 0
+	
+	--each coordinate table has four numbers
+	for i=1, 4 do
+		sum = sum + coordA[i]*coordB[i]
+		magnitudeA = magnitudeA + coordA[i]^2
+		magnitudeB = magnitudeB + coordB[i]^2
+	end
+	return sum / (math.sqrt(magnitudeA) * math.sqrt(magnitudeB))
+end
